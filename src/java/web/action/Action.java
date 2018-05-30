6fbaad0b48bb79e8d761ae6e69adb6f440a98c2e
Mads Fornes <mads.fornes@gmail.com>
2018-05-30 22:13:32 +0200

    dirtyfix

--- src/java/web/action/Action.java
+++
@@ -1,8 +0,0 @@
-package web.action;
-
-import javax.servlet.http.*;
-
-public abstract class Action {
-
-    public abstract void perform(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-}
